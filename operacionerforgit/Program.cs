d2d8d60c28b9c92cb90c3c83954aeea24cd8c59d
Jose Edgar <edgarin0903@hotmail.com>
2019-03-04 11:30:13 -0500

    configuracion inicial del proyecto

---
+++ operacionerforgit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operacionerforgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Operaciones Aritmeticas gon Git");
+            Console.WriteLine("Numero Uno");
+            short numuno = short.Parse(Console.ReadLine());
+            Console.WriteLine("Numero Dos");
+            short numdos = short.Parse(Console.ReadLine());
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
